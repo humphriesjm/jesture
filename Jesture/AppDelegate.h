de66a22bcbac4ecbf45c4123f5f5b5f1a3e78f1d
Jason Humphries <humphriesj@gmail.com>
2014-03-18 11:16:13 -0400

    Initial Commit

---
+++ Jesture/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Jesture
+//
+//  Created by Jason Humphries on 3/18/14.
+//  Copyright (c) 2014 PopUp Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Jesture/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Jesture
+//
+//  Created by Jason Humphries on 3/18/14.
+//  Copyright (c) 2014 PopUp Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
